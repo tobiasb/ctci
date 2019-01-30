437c3d13922cfa8a5eccf798a3854672dfb0dfbe
Tobias Boehm <boehm.tobi@gmail.com>
2019-01-29 22:45:49 -0800

    Ex 1.5 and 1.6

---
+++ Chapter 01/Exercise05.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ctci.Chapter_01
+{
+    public class Exercise05
+    {
+        [TestCase("pale", "ple")]
+        [TestCase("pales", "pale")]
+        [TestCase("pale", "bale")]
+        public void should_be_one_away(string s1, string s2)
+        {
+            AreOneAway(s1, s2).Should().BeTrue();
+        }
+
+        [TestCase("pale", "bake")]
+        [TestCase("p", "bake")]
+        [TestCase("pa", "bake")]
+        [TestCase("bake", "p")]
+        [TestCase("bake", "pa")]
+        public void should_not_be_one_away(string s1, string s2)
+        {
+            AreOneAway(s1, s2).Should().BeFalse();
+        }
+
+        private bool AreOneAway(string s1, string s2)
+        {
+            var shorter = s1.Length > s2.Length ? s2 : s1;
+            var longer = s1.Length > s2.Length ? s1 : s2;
+
+            int i = 0, j = 0;
+            var numDifferences = 0;
+
+            while(i < shorter.Length || j < longer.Length)
+            {
+                if(i >= shorter.Length || j >= longer.Length || shorter[i] != longer[j])
+                {
+                    if (numDifferences >= 1)
+                        return false;
+                    numDifferences++;
+
+                    if(longer.Length != shorter.Length)
+                        j++;
+                }
+
+                i++;
+                j++;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Chapter 01/Exercise06.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ctci.Chapter_01
+{
+    public class Exercise06
+    {
+        [TestCase("aabcccccaaa", "a2b1c5a3")]
+        [TestCase("abcde", "abcde")]
+        public void should_be(string original, string compressed)
+        {
+            Compress(original).Should().Be(compressed);
+        }
+
+        private string Compress(string original)
+        {
+            string compressed = "";
+            int lastNewChar = 0;
+
+            for(int i = 1; i <= original.Length; i++)
+            {
+                if(i == original.Length || original[i] != original[lastNewChar])
+                {
+                    compressed += original[lastNewChar] + (i - lastNewChar).ToString();
+
+                    lastNewChar = i;
+                }
+            }
+
+            if (compressed.Length <= original.Length)
+                return compressed;
+
+            return original;
+        }
+    }
+}
