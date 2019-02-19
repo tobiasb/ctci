aad396a14893f10b5b76ca151c3ee00911f7cc26
Tobias Boehm <boehm.tobi@gmail.com>
2019-02-18 19:10:15 -0800

    Ex 2.08

---
+++ Chapter 02/Exercise08.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ctci.Chapter_02
+{
+    public class Exercise08
+    {
+        [TestCase("abcdefg")]
+        public void should_return_null_when_no_loop(string input)
+        {
+            var root = StringNode.From(input);
+
+            GetFirstNodeOfLoop(root).Should().BeNull();
+        }
+
+        [TestCase("abcdefg", "a")]
+        public void should_return_first_node_in_loop(string input, string firstNodeStringValue)
+        {
+            var root = StringNode.From(input);
+            root.AppendToTail(root);
+
+            GetFirstNodeOfLoop(root).Value.Should().Be(firstNodeStringValue);
+        }
+
+        private Node<string> GetFirstNodeOfLoop(Node<string> root)
+        {
+            var nodeSet = new HashSet<Node<string>>();
+
+            for(var curr = root; curr != null; curr = curr.Next)
+            {
+                if(nodeSet.Contains(curr))
+                {
+                    return curr;
+                }
+
+                nodeSet.Add(curr);
+            }
+
+            return null;
+        }
+    }
+}
